98c1648c11d284b74c24b797513dbe808f236d06
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-11 14:38:32 -0600

    Added a new component for navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import Navbar from "./components/layouts/Navbar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello Jaime's App</h1>
-      </div>
+      <React.Fragment>
+        <Navbar></Navbar>
+      </React.Fragment>
     );
   }
 }
