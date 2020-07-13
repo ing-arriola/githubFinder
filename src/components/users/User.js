721e5522e0fc1be8787e8cb7dd912f1a785c77e8
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-12 19:33:44 -0600

    Now capable of search a single user and retrieve user data into the App without going to Github profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 
 export class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
   render() {
-    return <div>User</div>;
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      public_repos,
+      public_gists,
+    } = this.props.user;
+
+    const { loading } = this.props;
+    return <div>{name}</div>;
   }
 }
 
