fcf724087ed3af169c383674f8b5bc7699e54786
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-17 03:15:12 -0600

    completed alertContex.js, alertReducer.js and Alert.js

--- src/context/alert/alertState.js
+++ src/context/alert/alertState.js
@@ -1,12 +1,21 @@
 import React, { useReducer } from "react";
-import axios from "axios";
 import AlertContext from "./alertContext";
 import AlertReducer from "./alertReducer";
-import { SET_ALERT, REMOVE_ALERT } from "../types";
+import { SET_ALERT, REMOVE_ALERT, SET_LOADING } from "../types";
 
 const GithubState = (props) => {
   const initialState = null;
   const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+  // Set Alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type },
+    });
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT }), 2000);
+  };
 
   return (
     <AlertContext.Provider
