ab8ecb6c373dd8a585528c9a1e84a32e0e9419a4
ing-arriola <ingeniero.arriola@gmail.com>
2020-07-16 20:01:16 -0600

    Added files to AlertState

---
+++ src/context/alert/alertState.js
@@ -0,0 +1,23 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import AlertContext from "./alertContext";
+import AlertReducer from "./alertReducer";
+import { SET_ALERT, REMOVE_ALERT } from "../types";
+
+const GithubState = (props) => {
+  const initialState = null;
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alert: state,
+        setAlert,
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertState;
