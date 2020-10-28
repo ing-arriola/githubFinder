346567b0880beeb4529fceac534e7e1988bd9569
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-28 10:17:04 -0600

    Updated readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-GITHUB FINDER 
+# GITHUB FINDER
 
-This project uses the GITHUB-API in order to get info from existing users on GITHUB :)
+## Developed by Jaime Arriola
 
-Building block:
-REACT-JS
-AXIOS
+- Situation: I wanted to improve my skills at getting data from an API
+
+- Action: I followed a tutorial to consume the Github API using REACT-JS, AXIOS and also context API.
+
+- Result: I developed this project to get public data from Github profiles so, this project helped me to improve my knowledge on how to get data from an API.
---
+++ build/precache-manifest.a2b97575c82876d94723792be5f28c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cef422dd3de1c4849e54e1599ce368c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "645a82c52dc4de595b8d",
+    "url": "/static/css/main.16cd0b2e.chunk.css"
+  },
+  {
+    "revision": "24aa5735cad22b5405cb",
+    "url": "/static/js/2.a4cef872.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a4cef872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645a82c52dc4de595b8d",
+    "url": "/static/js/main.f2514660.chunk.js"
+  },
+  {
+    "revision": "7dbaecb9b1566e9ab509",
+    "url": "/static/js/runtime-main.9847c3b2.js"
+  }
+]);
